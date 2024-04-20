8118a607b45298d9cffe0a7a5aa41b5b83c8647b
athoscastelo <you@example.com>
2024-04-19 22:04:54 -0300

    atualizacao imports

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,23 +1,23 @@
 INSERT INTO marca (id, nome) VALUES
-(1, 'Adidas'),
-(2, 'Nike'),
-(3, 'Puma');
+(1, 'Calvin Klein'),
+(2, 'Hugo Boss'),
+(3, 'Gucci');
 
 INSERT INTO modelo (id, nome) VALUES
-(1, 'Superstar'),
-(2, 'Air Force 1'),
-(3, 'Suede');
+(1, 'Oxford'),
+(2, 'Monk Strap'),
+(3, 'Derby');
 
 INSERT INTO cor (id, nome) VALUES
-(1, 'Preto'),
-(2, 'Branco'),
-(3, 'Vermelho');
+(1, 'Marrom'),
+(2, 'Bege'),
+(3, 'Azul Marinho');
 
 INSERT INTO fornecedor (id, nome, cnpj, endereco, telefone) VALUES
-(1, 'Fornecedor A', '1234567890001', 'Rua A, 123', '(11) 1234-5678'),
-(2, 'Fornecedor B', '1234567890002', 'Rua B, 456', '(22) 9876-5432');
+(1, 'Fornecedor C', '1234567890003', 'Rua C, 789', '(33) 1234-5678'),
+(2, 'Fornecedor D', '1234567890004', 'Rua D, 1011', '(44) 9876-5432');
 
-INSERT INTO sapato (numeracao_id, preco, estoque, fornecedor_id, marca_id, cor_id, modelo_id) VALUES
-(1, 199.99, 50, 1, 1, 1, 1),
-(2, 149.99, 30, 1, 2, 2, 2),
-(3, 129.99, 20, 2, 3, 3, 3);
+INSERT INTO sapato (numeracao_id, fornecedor_id, marca_id, cor_id, modelo_id) VALUES
+(1, 1, 1, 1, 1),
+(2, 1, 2, 2, 2),
+(3, 2, 3, 3, 3);
